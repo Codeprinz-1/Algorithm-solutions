5a7dc7cd09244a6ed077af6281c97654b5d45a8f
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:18:00 +0100

    addded description for someRecursive

---
+++ someRecursive.js
@@ -0,0 +1,1 @@
+// Task: write a recursive function which accepts an array and a callback. The function returns true if a simgle value in the array returns tru when passed to the callback. Otherwise it returns true.
