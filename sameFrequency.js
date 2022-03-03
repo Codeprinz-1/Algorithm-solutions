5cb3200e4bb9bb39323ebae7eaa267af02175ba4
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 13:01:35 +0100

    added solution for same frequency

--- sameFrequency.js
+++ sameFrequency.js
@@ -1,1 +1,20 @@
 // Task: write a function, Given two positive integers, find out if the two numbers have the same frequency of digits
+
+const sameFrequency = (int1, int2) => {
+  int1 = String(int1);
+  int2 = String(int2);
+  if (int1.length !== int2.length) return false;
+  let frequencyCounter = {};
+
+  for (let char of String(int1)) {
+    frequencyCounter[char] = ++frequencyCounter[char] || 1;
+  }
+
+  for (let char of String(int2)) {
+    if (!frequencyCounter[char]) return false;
+    else {
+      --frequencyCounter[char];
+    }
+  }
+  return true;
+};
