7e63d6e2d2a60a139df510a112fcc925b59f1bbe
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:26:31 +0100

    added solution for nestedevensum

--- nestedEvenSum.js
+++ nestedEvenSum.js
@@ -1,1 +1,14 @@
 // write a function . Return the sum o fall the even numbers in an object which may contain nested objects
+
+const nestedEvenSum = (obj) => {
+  let sum = 0;
+  for (let key in obj) {
+    if (typeof obj[key] === "object") sum += nestedEvenSum(obj[key]);
+    else if (typeof obj[key] === "number") {
+      if (obj[key] % 2 === 0) {
+        sum += obj[key];
+      }
+    }
+  }
+  return sum;
+};
