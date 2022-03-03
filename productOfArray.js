3a2d422d66e0e3462a5a340455e2e34fc2c9ede4
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:03:16 +0100

    added product of array solution

--- productOfArray.js
+++ productOfArray.js
@@ -1,1 +1,6 @@
 // write a function which takes in an array of numbers and returns the product of them all
+
+const productOfArray = (arr) => {
+  if (arr.length === 1) return arr[0];
+  return arr[0] * productOfArray(arr.slice(1));
+};
