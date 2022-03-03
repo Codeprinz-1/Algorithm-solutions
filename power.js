d8953bf211db9447ccb26fee884bafbf4f6bcc9f
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 14:57:16 +0100

    aded solution for power

--- power.js
+++ power.js
@@ -1,2 +1,7 @@
 // Task: write a function which accepts a base and an exponent. The function should return the power of the base  to the exponent.
 // This function should mimic the functionality of Math.pow() - do not worry about bases and exponents
+
+const power = (num, pow) => {
+  if (pow === 0) return 1;
+  return num * power(num, pow - 1);
+};
