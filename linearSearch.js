3d5fd37eac16987f152245ed1e802c6d8bf2a372
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 18:56:16 +0100

    aded solution for linear search

--- linearSearch.js
+++ linearSearch.js
@@ -1,1 +1,10 @@
 // Task: write a functoin which accepts an array and a value and returns the index at which the value exists. If the value does not exist in the array, return -1
+
+const linearSearch = (arr, num) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return i;
+    }
+  }
+  return -1;
+};
