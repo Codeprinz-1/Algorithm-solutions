0d393f6af7e4876182a05e5c28414a0d3d7ac88b
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:07:01 +0100

    added solution for recursive range

--- recursiveRange.js
+++ recursiveRange.js
@@ -1,1 +1,6 @@
 // Task: write a function which accepts a number and adds up all the numbers from 0 to the number passed to the function
+
+const recursiveRange = (num) => {
+  if (num === 0) return 0;
+  return num + recursiveRange(num - 1);
+};
