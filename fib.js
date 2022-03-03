6f788e4f6085a5e01adcc6b976e77dcfcbf09bec
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:09:22 +0100

    added description for fib

---
+++ fib.js
@@ -0,0 +1,1 @@
+// write a function  which accepts a number and returns the nth number in the fibonacce sequence. Recall that the fiibonacce sequence is the sequence of whole numbers 1,1,2,3,5,8 .... whihc strts with 1 and 1, and where every number thereafter is equal to the sum of the previous two numbers.
