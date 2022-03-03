91cd8dde0db1e1b0d8e9f4cdbeb84b7fb946a562
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:12:03 +0100

    added solution or recursiverange

--- reverse.js
+++ reverse.js
@@ -1,1 +1,6 @@
 // write a function which accepts a string and returns a new string in reverse
+
+function recursiveRange(num) {
+  if (num === 0) return 0;
+  return num + recursiveRange(num - 1);
+}
