5069bbcecbb9f76840ebeb8af67d931b8a731826
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:21:20 +0100

    added solution for flatten

--- flatten.js
+++ flatten.js
@@ -1,1 +1,13 @@
 // Task: write a function whihc accepts an array of arrays and returns a new array with all values flattened.
+
+const flatten = (arr) => {
+  var newArray = [];
+  for (let i of arr) {
+    if (Array.isArray(i)) {
+      newArray = newArray.concat(flatten(i));
+    } else {
+      newArray.push(i);
+    }
+  }
+  return newArray;
+};
