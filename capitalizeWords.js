4fc9f96b0ba5978de59ae93b961f634d4f76a1c2
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:28:49 +0100

    added solution for  capitalizewords

--- capitalizeWords.js
+++ capitalizeWords.js
@@ -1,1 +1,8 @@
 // Task: write a function. Given an array of words, return a new array contianing each word capitalized.
+
+const capitalizeWords = (arr) => {
+  if (arr.length === 1) return [arr[0].toUpperCase()];
+  const res = capitalizeWords(arr.slice(0, -1));
+  res.push(arr.slice(arr.length - 1)[0].toUpperCase());
+  return res;
+};
