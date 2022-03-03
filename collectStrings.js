18789451abdd18d00c288b9f12d3e0d7c6fdc5ec
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:34:57 +0100

    added solution for collectStrings

--- collectStrings.js
+++ collectStrings.js
@@ -1,1 +1,12 @@
 // Task: write a function which accepts an object and returns an array of all the values in the object that have a typeof string
+
+const collectStrings = (obj) => {
+  let strings = [];
+  for (let key in obj) {
+    if (typeof obj[key] === "string") strings.push(obj[key]);
+    else if (obj[key].constructor === Object) {
+      strings = strings.concat(collectStrings(obj[key]));
+    }
+  }
+  return strings;
+};
