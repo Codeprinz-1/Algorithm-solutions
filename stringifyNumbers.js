d6a26c3dd665959bfb504935ed0cfa15b58801f2
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 15:32:08 +0100

    added solutiion for stingifyNumbers

--- stringifyNumbers.js
+++ stringifyNumbers.js
@@ -1,1 +1,13 @@
 // Task: write a function called which takes in an object and finds all of the values which are numbers and coverts them to strings. Recursion would be a great way to solve this!
+
+const stringifyNumbers = (obj) => {
+  let tempObj = Object.assign({}, obj);
+  for (let key in tempObj) {
+    if (tempObj[key].constructor === Object) {
+      tempObj[key] = stringifyNumbers(tempObj[key]);
+    } else if (typeof tempObj[key] === "number") {
+      tempObj[key] = `${tempObj[key]}`;
+    }
+  }
+  return tempObj;
+};
