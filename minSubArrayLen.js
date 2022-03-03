26a2038b6835843978252cc514ee0f552c2a9f23
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 14:32:50 +0100

    added description for min sub aray len

---
+++ minSubArrayLen.js
@@ -0,0 +1,2 @@
+// Task: write a function which accepts two parameters, an array of positive integers and a positive integer
+// This function should return the minimal length of a contiguous subarray or which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead
