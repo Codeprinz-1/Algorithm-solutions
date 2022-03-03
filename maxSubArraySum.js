1f78c53e4df6662a43ee065fca5071cb4497db39
Codeprince <anyaohaprince122@gmail.com>
2022-03-03 14:26:50 +0100

    aded max sub array sum

--- maxSubArraySum.js
+++ maxSubArraySum.js
@@ -1,1 +1,20 @@
 // Task: Given an array of integers and a number, write function, which finds the maximum sum of a subarray with the length  of the number passed to the function.
+
+const maxSubarraySum = (array, num) => {
+  if (array.length < num) return null;
+  let max = 0;
+  let tempmax = 0;
+
+  for (let i = 0; i < num; i++) {
+    max += array[i];
+  }
+
+  tempmax = max;
+
+  for (let i = num; i < array.length; i++) {
+    tempmax = tempmax + array[i] - array[i - num];
+
+    max = Math.max(tempmax, max);
+  }
+  return max;
+};
