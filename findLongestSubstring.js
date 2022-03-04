06109045e18e69c812285e205dcd7c05afce1fac
Codeprince <anyaohaprince122@gmail.com>
2022-03-04 22:06:55 +0100

    added funtion for findlongestsubstring

--- findLongestSubstring.js
+++ findLongestSubstring.js
@@ -1,1 +1,17 @@
 // write a function which accepts a string and rreturns the length of the longest substring with distinct characters
+
+const findLongestSubstring = (str) => {
+  let start = 0;
+  let seen = {};
+  let longest = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+  }
+  return longest;
+};
