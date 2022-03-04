48717162b0c010d8044b91d84b28d2ba5011e7f8
Codeprince <anyaohaprince122@gmail.com>
2022-03-04 09:49:04 +0100

    added solution for is subsequence

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,2 +1,16 @@
 // write a function which takes in two strings and checks whether the characters in the first string for a subsequence of the characters in the second strind.
 // In other words, the function should check whether the characters in the first string appear somewhere in the second string without their order changing.
+
+const isSubsequence = (str1, str2) => {
+  let pointer1 = 0;
+  let pointer2 = str1.length;
+
+  while (pointer2 <= str2.length) {
+    if (str2.substring(pointer1, pointer2) === str1) return true;
+    else {
+      pointer1++;
+      pointer2++;
+    }
+  }
+  return false;
+};
