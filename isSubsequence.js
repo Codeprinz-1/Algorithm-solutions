30828ea9814ce497fcd806efb3de32587d99c205
Codeprince <anyaohaprince122@gmail.com>
2022-03-04 09:41:34 +0100

    added description for is subsequence

---
+++ isSubsequence.js
@@ -0,0 +1,2 @@
+// write a function which takes in two strings and checks whether the characters in the first string for a subsequence of the characters in the second strind.
+// In other words, the function should check whether the characters in the first string appear somewhere in the second string without their order changing.
