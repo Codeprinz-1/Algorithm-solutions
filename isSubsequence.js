716bbd10a150e25956c5536a6c00bbb31d00fcd8
Codeprince <anyaohaprince122@gmail.com>
2022-03-04 12:04:24 +0100

    fixed solution for subsequence

--- isSubsequence.js
+++ isSubsequence.js
@@ -3,14 +3,12 @@
 
 const isSubsequence = (str1, str2) => {
   let pointer1 = 0;
-  let pointer2 = str1.length;
+  let pointer2 = 0;
 
-  while (pointer2 <= str2.length) {
-    if (str2.substring(pointer1, pointer2) === str1) return true;
-    else {
-      pointer1++;
-      pointer2++;
-    }
+  while (pointer2 < str2.length) {
+    if (str1[pointer1] === str2[pointer2]) pointer1++;
+    if (pointer1 === str1.length) return true;
+    pointer2++;
   }
   return false;
 };
